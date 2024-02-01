a9a3b8e0e4dc07bda2de7088e0d8c57fd53ec2a2
Bakhtiyar Ismayil <bakhtiyar.ismayil@gmail.com>
2024-02-01 17:09:39 +0400

    Thu Feb  1 05:09:39 PM +04 2024

--- haproxy.sh
+++ haproxy.sh
@@ -41,14 +41,90 @@ docker run -d -p 80 --net mynetwork --name nginx1 nginx
 
 #docker exec -it nginx1 bash -c 'apt update && apt install -y vim' > output.log 2>&1 &
 
-docker exec -it nginx1 sh -c 'echo "NGINX1" >> /usr/share/nginx/html/index.html'
+docker exec -it nginx1 sh -c 'cat <<EOF > /usr/share/nginx/html/index.html
+<!DOCTYPE html>
+<html>
+<head>
+<title>Welcome to NGINX 1 </title>
+<style>
+html { color-scheme: light dark; }
+body { width: 35em; margin: 0 auto;
+font-family: Tahoma, Verdana, Arial, sans-serif; }
+</style>
+</head>
+<body>
+<h1>Welcome to NGINX 1</h1>
+<p>If you see this page, the nginx web server is successfully installed and
+working. Further configuration is required.</p>
+
+<p>For online documentation and support please refer to
+<a href="http://nginx.org/">nginx.org</a>.<br/>
+Commercial support is available at
+<a href="http://nginx.com/">nginx.com</a>.</p>
+
+<p><em>Thank you for using nginx.</em></p>
+</body>
+</html>
+EOF'
+
+
 
 docker run -d -p 80 --net mynetwork --name nginx2 nginx
 
-docker exec -it nginx2 sh -c 'echo "NGINX2" >> /usr/share/nginx/html/index.html'
+docker exec -it nginx2 sh -c 'cat <<EOF > /usr/share/nginx/html/index.html
+<!DOCTYPE html>
+<html>
+<head>
+<title>Welcome to NGINX 2 </title>
+<style>
+html { color-scheme: light dark; }
+body { width: 35em; margin: 0 auto;
+font-family: Tahoma, Verdana, Arial, sans-serif; }
+</style>
+</head>
+<body>
+<h1>Welcome to NGINX 2 </h1>
+<p>If you see this page, the nginx web server is successfully installed and
+working. Further configuration is required.</p>
+
+<p>For online documentation and support please refer to
+<a href="http://nginx.org/">nginx.org</a>.<br/>
+Commercial support is available at
+<a href="http://nginx.com/">nginx.com</a>.</p>
+
+<p><em>Thank you for using nginx.</em></p>
+</body>
+</html>
+EOF'
+
+
 
 docker run -d -p 80 --net mynetwork --name nginx3 nginx
 
-docker exec -it nginx3 sh -c 'echo "NGINX3" >> /usr/share/nginx/html/index.html'
+docker exec -it nginx3 sh -c 'cat <<EOF > /usr/share/nginx/html/index.html
+<!DOCTYPE html>
+<html>
+<head>
+<title>Welcome to NGINX 3 </title>
+<style>
+html { color-scheme: light dark; }
+body { width: 35em; margin: 0 auto;
+font-family: Tahoma, Verdana, Arial, sans-serif; }
+</style>
+</head>
+<body>
+<h1>Welcome to NGINX 3</h1>
+<p>If you see this page, the nginx web server is successfully installed and
+working. Further configuration is required.</p>
+
+<p>For online documentation and support please refer to
+<a href="http://nginx.org/">nginx.org</a>.<br/>
+Commercial support is available at
+<a href="http://nginx.com/">nginx.com</a>.</p>
+
+<p><em>Thank you for using nginx.</em></p>
+</body>
+</html>
+EOF'
 
 docker run -d --name haproxy --net mynetwork -v $(pwd):/usr/local/etc/haproxy:ro -p 80:80 -p 8404:8404 haproxytech/haproxy-alpine # or just haproxy
